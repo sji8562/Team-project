666e9b5477af62bfd517adfcdcbd91692fa7dd09
YHYELIM <yhl0513@naver.com>
2023-09-06 17:16:07 +0900

    회사정보수정

--- src/main/java/shop/mtcoding/teamproject/company/CompanyController.java
+++ src/main/java/shop/mtcoding/teamproject/company/CompanyController.java
@@ -53,4 +53,19 @@ public class CompanyController {
         response.sendRedirect("/");
     }
 
+    @PostMapping("/companyupdateForm")
+    public String update(CompanyRequest.UpdateDTO updateDTO) {
+
+        Company sessionCompany = (Company) session.getAttribute("sessionCompany");
+        Company company = companyService.기업정보수정(updateDTO, sessionCompany.getIndex());
+        session.setAttribute("sessionCompany", company);
+        System.out.println("++++++++++++++비번바꿨다우");
+        return "redirect:/companyLoginForm";
+
+    }
+
 }
+// 수정시에는 영속성 컨텍스트 User오브젝트를 영속화시키고 영속화git 된 User 오브젝트를 수정
+// select해서 User오브젝트를 디비로 부터 가져오는 이유는 영속화를 하기 위해서
+// 영속화된 오브젝트를 변경하면 자동으로 디비에 업데이트문을 날려주거든요
+// 영속화된 persistance 객체의 변화가 감지되면 더티 체킹
