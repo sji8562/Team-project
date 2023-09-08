8b8450cebb22f10554a10c6d007ebf9e2f05e08a
heidi505 <xxwhite19@gmail.com>
2023-09-08 15:33:35 +0900

    지원 등록 완료

---
+++ src/main/java/shop/mtcoding/teamproject/apply/ApplyRepository.java
@@ -0,0 +1,8 @@
+package shop.mtcoding.teamproject.apply;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplyRepository extends JpaRepository<Apply, Integer>{
+
+}
+    
---
+++ src/main/java/shop/mtcoding/teamproject/apply/ApplyService.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.teamproject.apply;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import shop.mtcoding.teamproject.announcement.Announcement;
+import shop.mtcoding.teamproject.resume.Resume;
+import shop.mtcoding.teamproject.user.User;
+import shop.mtcoding.teamproject.user.UserRepository;
+import shop.mtcoding.teamproject.user.UserService;
+
+@Service
+public class ApplyService {
+    @Autowired
+    private ApplyRepository applyRepository;
+    @Autowired
+    private HttpSession session;
+    @Autowired
+    private UserRepository userRepository;
+
+    public void 지원등록(Resume resume, Announcement announcement){
+        User user = (User) session.getAttribute("sessionUser");
+
+        Apply apply = Apply.builder()
+                    .user(userRepository.findById(user.getIndex()).get())
+                    .resume(resume)
+                    .announcement(announcement)
+                    .status(3)
+                    .build();
+
+        applyRepository.save(apply);
+    }
+}
