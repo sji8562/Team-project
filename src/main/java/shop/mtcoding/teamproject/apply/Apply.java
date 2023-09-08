7d65ac217abc484559d1197e3d1c2e445780a01b
heidi505 <xxwhite19@gmail.com>
2023-09-08 15:33:35 +0900

    지원 등록 완료

--- src/main/java/shop/mtcoding/teamproject/apply/Apply.java
+++ src/main/java/shop/mtcoding/teamproject/apply/Apply.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.teamproject.apply;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,15 +28,26 @@ public class Apply {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer index;
     
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Announcement announcement;
     
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Resume resume;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private User user;
 
     @Column(nullable = false)
     private Integer status;
+
+    @Builder
+    public Apply(Integer index, Announcement announcement, Resume resume, User user, Integer status) {
+        this.index = index;
+        this.announcement = announcement;
+        this.resume = resume;
+        this.user = user;
+        this.status = status;
+    }
+
+    
 }
--- src/main/java/shop/mtcoding/teamproject/apply/ApplyController.java
+++ src/main/java/shop/mtcoding/teamproject/apply/ApplyController.java
@@ -1,11 +1,29 @@
 package shop.mtcoding.teamproject.apply;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import shop.mtcoding.teamproject._core.error.ex.MyApiException;
+import shop.mtcoding.teamproject._core.util.ApiUtil;
+import shop.mtcoding.teamproject.announcement.Announcement;
+import shop.mtcoding.teamproject.resume.Resume;
 
 @Controller
 public class ApplyController {
-    
+    @Autowired
+    private ApplyService applyService;
+
+    @PostMapping("/api/apply/save")
+    public @ResponseBody ApiUtil<String> applySave(Resume resume, Announcement announcement){
+        try {
+            applyService.지원등록(resume, announcement);
+            return new ApiUtil<String>(true, "지원이 완료되었습니다");
+        } catch (MyApiException e) {
+            throw new MyApiException("지원이 완료되지 않았습니다");
+        }
+    }
    
 }
