4751444a26fc660556e205a121a09780d910b8b5
heidi505 <xxwhite19@gmail.com>
2023-09-09 13:12:35 +0900

    지원 - 자바스크립트에서 announcement가 주소값 전달되는 오류

--- src/main/java/shop/mtcoding/teamproject/apply/Apply.java
+++ src/main/java/shop/mtcoding/teamproject/apply/Apply.java
@@ -7,6 +7,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -29,12 +30,15 @@ public class Apply {
     private Integer index;
     
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "announcement_index")
     private Announcement announcement;
     
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "resume_index")
     private Resume resume;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_index")
     private User user;
 
     @Column(nullable = false)
--- src/main/java/shop/mtcoding/teamproject/apply/ApplyService.java
+++ src/main/java/shop/mtcoding/teamproject/apply/ApplyService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import shop.mtcoding.teamproject.announcement.Announcement;
+import shop.mtcoding.teamproject.announcement.AnnouncementRepository;
 import shop.mtcoding.teamproject.resume.Resume;
+import shop.mtcoding.teamproject.resume.ResumeRepository;
 import shop.mtcoding.teamproject.user.User;
 import shop.mtcoding.teamproject.user.UserRepository;
 import shop.mtcoding.teamproject.user.UserService;
@@ -19,10 +21,13 @@ public class ApplyService {
     private HttpSession session;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AnnouncementRepository announcementRepository;
+    @Autowired
+    private ResumeRepository resumeRepository;
 
     public void 지원등록(Resume resume, Announcement announcement){
         User user = (User) session.getAttribute("sessionUser");
-
         Apply apply = Apply.builder()
                     .user(userRepository.findById(user.getIndex()).get())
                     .resume(resume)
